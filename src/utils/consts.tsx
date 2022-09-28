0cf638067d18df1993880d392dd1e13f2b749755
RyotaAraya <camerondiaz1215@gmail.com>
2022-09-28 22:10:36 +0900

    プラント追加

--- src/utils/consts.tsx
+++ src/utils/consts.tsx
@@ -4,9 +4,13 @@ import type { Item } from 'components/molecules/FilterGroup'
 export const PLANTS: DropdownItem[] = [
   { label: '1号ボイラー', value: '1BH' },
   { label: '2号ボイラー', value: '2BH' },
+  { label: '1号発電機', value: '1SP' },
+  { label: '2号発電機', value: '2SP' },
 ]
 
 export const PLANTS2: Item[] = [
   { label: '1号ボイラー', name: '1BH' },
   { label: '2号ボイラー', name: '2BH' },
+  { label: '1号発電機', name: '1SP' },
+  { label: '2号発電機', name: '2SP' },
 ]
